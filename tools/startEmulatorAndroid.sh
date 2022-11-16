407faf7e9adeee186b7cc790473cd1740c68c7fc
erdira <stefankarlin@hotmail.com>
2022-11-16 20:31:42 +0100

    Add helper script

---
+++ tools/startEmulatorAndroid.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#to list available avds run: emulator --list-avds
+
+# pass dns server arg to be able to connect to the internet
+/Users/stefan/Library/Android/sdk/emulator/emulator -dns-server 8.8.8.8 -avd Pixel_3a_XL_API_30
